2f69ebf5824468e75c80f1f70c30b5e900934027
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-14 09:59:29 -0600

    more model data added

--- models/Event.js
+++ models/Event.js
@@ -22,6 +22,13 @@ Event.init(
         key: 'id',
       },
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
